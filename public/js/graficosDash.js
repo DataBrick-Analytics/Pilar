8501e9df380d654b8808819d8937ab504ddd89d4
Gusta-PA <gustavo.almeida@sptech.school>
2025-03-31 10:24:52 -0300

    feat: charts addition

--- public/js/graficosDash.js
+++ public/js/graficosDash.js
@@ -1,24 +1,40 @@
-const chartlinha = document.getElementById('chartLine');
-
-const chart = new Chart(chartlinha, {
-    type: "line",
+const chartLine = document.getElementById('chartLine');
+new Chart(chartLine, {
+    type: 'line',
     data: {
-        labels: XXXXX,
-        datasets: [
-            {
-                label: 'My First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            },
-        ],
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true,
-            },
-        },
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+const chartPie = document.getElementById('chartPie')
+new Chart(chartPie, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
     },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 })
