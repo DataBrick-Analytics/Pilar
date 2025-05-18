e5c85e49d22c3ddbcaf8a99493c184136c8d8f88
macari-11 <macari.pinho@sptech>
2025-05-18 20:32:42 -0300

    feat: botão-LogOut[

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,4 +4,11 @@ const dataAgora = new Date();
 const formatado = dataAgora.toLocaleString("pt-BR");
 
 document.getElementById("ultimaAtualizacao").textContent = `Atualizado em: ${formatado}`;
-document.getElementById("ultimaAtualizacao2").textContent = `Atualizado em: ${formatado}`;;
+document.getElementById("ultimaAtualizacao2").textContent = `Atualizado em: ${formatado}`;
+
+function logout(){
+    localStorage.clear();
+    sessionStorage.clear();
+
+    window.location.href = "./index.html"
+}
