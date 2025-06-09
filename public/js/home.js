f0621d981343b74ebe84cd473c1b915b7c822036
macari-11 <macari.pinho@sptech>
2025-06-09 19:47:53 -0300

    funcionalidades-botões

---
+++ public/js/home.js
@@ -0,0 +1,24 @@
+fetch("/data/getRegiaoRecomendada")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach((regiao, index) => {
+            const regiaoId = document.getElementById(`regiao${index + 1}`);
+            const zonaId = document.getElementById(`zona${index + 1}`);
+            if (regiaoId && zonaId) {
+                regiaoId.textContent = regiao.nome_distrito; 
+                zonaId.textContent = regiao.zona; 
+            }
+        });
+    })
+    .catch(error => {
+        console.error("Erro ao carregar regiões recomendadas:", error);
+    });
+
+
+    document.getElementById('button-forms').addEventListener('click', function() {
+    window.location.href = 'formulario.html'; 
+  });
+
+  document.getElementById('button-allerts').addEventListener('click', function() {
+    window.location.href = 'TelaAlertas.html'; 
+  });
